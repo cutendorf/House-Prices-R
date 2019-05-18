40e56bbd006102e19d7a6a3a091daa049c0343f5
cutendorf <christine.utendorf@student.ie>
2019-05-18 13:09:06 +0200

    Update raw_code.R
    Fixing data types

--- raw_code.R
+++ raw_code.R
@@ -1,8 +1,32 @@
 source('functions/libraries.R')
 source('functions/functions.R')
+source('functions/metrics.R')
 
 raw_train_data<-fread('data/house_price_train.csv', stringsAsFactors = F)
 
 raw_test_data<-fread('data/house_price_test.csv', stringsAsFactors = F)
 
-dataset = f_split(raw_train_data, raw_test_data, testsize = 0.2, seed = 1)
+str(raw_train_data)
+
+sum(is.na(raw_train_data))
+sum(is.na(raw_test_data))
+
+cols_train<-names(raw_train_data)
+cols_test<-names(raw_test_data)
+
+raw_train_data$date<-as.Date(raw_train_data$date, "%m/%d/%Y")
+raw_test_data$date<-as.Date(raw_test_data$date, "%m/%d/%Y")
+
+raw_train_data$zipcode<-as.factor(raw_train_data$zipcode)
+raw_test_data$zipcode<-as.factor(raw_test_data$zipcode)
+
+raw_train_data$waterfront<-as.logical(raw_train_data$waterfront)
+raw_test_data$waterfront<-as.logical(raw_test_data$waterfront)
+
+raw_train_data[ , names(raw_train_data)[sapply(raw_train_data, is.integer)]:=lapply(.SD,as.numeric),
+           .SDcols = sapply(raw_train_data, is.integer)]
+raw_test_data[ , names(raw_test_data)[sapply(raw_test_data, is.integer)]:=lapply(.SD,as.numeric),
+           .SDcols = sapply(raw_test_data, is.integer)]
+
+
+#dataset = f_split(raw_train_data, raw_test_data, testsize = 0.2, seed = 1)
