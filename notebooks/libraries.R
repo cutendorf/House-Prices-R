6d7256155e89f9d2378ac821b625d39673159afa
cutendorf <christine.utendorf@student.ie>
2019-05-21 16:10:08 +0200

    Create libraries.R
    renaming folder from functions to notebooks

---
+++ notebooks/libraries.R
@@ -0,0 +1,16 @@
+# data munging libraries
+library(data.table)
+library(lubridate)
+library(DT)
+
+# plotting libraries
+library(ggplot2)
+options(scipen = 999)
+theme_set(theme_minimal(base_size = 16))
+library(plotly)
+#library(ggrepel)
+#library(ggiraph)
+
+# machine learning libraries
+library(caret)
+library(ranger)
