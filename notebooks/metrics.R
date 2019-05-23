cc2dcbc2272821e0c7ef8d7d44c06f35ed8a4b69
cutendorf <christine.utendorf@student.ie>
2019-05-23 17:12:14 +0200

    Improve model

--- notebooks/metrics.R
+++ notebooks/metrics.R
@@ -1,11 +1,14 @@
+#Mean absolute error
 mae<-function(real, predicted){
   return(mean(abs(real-predicted)))
 }
 
+#Mean absolute percentage error
 mape<-function(real,predicted){
   return(mean(abs((real-predicted)/real)))
 }
 
+#Root mean squared error
 rmse<-function(real,predicted){
   return(sqrt(mean((real-predicted)^2)))
 }
